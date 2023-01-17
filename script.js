2eeb869688147cebf1f0163a7c704873f07163aa
André Marques <andrelcm89pt@gmail.com>
2023-01-17 11:51:54 +0000

    product details added

--- script.js
+++ script.js
@@ -19,5 +19,21 @@ function closeMenu() {
 }
 
 
+// Change product details photo
 
+const mainImg = document.getElementById('mainImg')
+const smallImg = document.getElementsByClassName('smallImg')
+
+smallImg[0].onclick = function() { // Using an array for the "smallImg" class, changing the source of the mainImg on clicking in the element for the element in the array positions
+    mainImg.src = smallImg[0].src
+}
+smallImg[1].onclick = function() {
+    mainImg.src = smallImg[1].src
+}
+smallImg[2].onclick = function() {
+    mainImg.src = smallImg[2].src
+}
+smallImg[3].onclick = function() {
+    mainImg.src = smallImg[3].src
+}
 
